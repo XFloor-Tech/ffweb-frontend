8df655f5824c08ae34c3f3f97f6627f2fbcebbcd
Golubev <minecraft.danik@mail.ru>
2026-01-10 15:38:16 +0300

    feat: file dropzone init

--- src/app/queries.ts
+++ src/app/queries.ts
@@ -2,7 +2,7 @@ import { queryOptions } from "@tanstack/react-query";
 
 import { apiRequest } from "@/lib/api-client";
 
-const appQueryKeys = {
+const uploadQueryKeys = {
   health: () => ["health"],
 } as const;
 
@@ -17,7 +17,7 @@ type HealthResponse = {
 
 const healthQueryOptions = () =>
   queryOptions({
-    queryKey: appQueryKeys.health(),
+    queryKey: uploadQueryKeys.health(),
     queryFn: async () => {
       const [data, error] = await apiRequest<HealthResponse>({
         method: "GET",
--- src/pages/upload/index.tsx
+++ src/pages/upload/index.tsx
@@ -7,12 +7,12 @@ type Props = {};
 
 const UploadPage: FC<Props> = () => {
   return (
-    <div className="flex flex-col gap-4 text-white lg:flex-row">
-      <section className="min-h-screen w-full lg:w-[960px] lg:shrink-0">
+    <div className="flex gap-4 text-white">
+      <section className="min-h-screen w-[960px] shrink-0">
         <FileUpload />
       </section>
 
-      <section className="hidden min-h-screen w-full lg:block">
+      <section className="min-h-screen w-full">
         <UploadSettings />
       </section>
     </div>
