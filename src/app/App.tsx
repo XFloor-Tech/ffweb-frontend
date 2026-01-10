5cad1051a875f5203aa8db1d21cda3c9cebc06c4
Golubev <minecraft.danik@mail.ru>
2026-01-10 19:13:08 +0300

    feat: fix loading app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,13 @@
+import { useQuery } from "@tanstack/react-query";
 import type { FC } from "react";
 import { useEffect, useRef } from "react";
-import { useQuery } from "@tanstack/react-query";
 import { toast } from "sonner";
 
 import { healthQueryOptions } from "@/app/queries";
 import { Header } from "@/components/layout/header";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Toaster } from "@/components/ui/sonner";
+import { Spinner } from "@/components/ui/spinner";
 import { UploadPage } from "@/pages/upload";
 
 const App: FC = () => {
@@ -26,17 +27,12 @@ const App: FC = () => {
     }
   }, [isError]);
 
-  if (!isHealthy) {
+  if (isLoading) {
     return (
       <>
         <Toaster />
         <div className="min-h-screen px-4 py-4 2xl:px-64 md:px-12 md:py-6 lg:px-48">
-          <Alert variant="destructive">
-            <AlertTitle>We couldn't reach the server</AlertTitle>
-            <AlertDescription>
-              Please start the mock server and reload the page.
-            </AlertDescription>
-          </Alert>
+          <Spinner className="text-primary" />
         </div>
       </>
     );
@@ -49,7 +45,16 @@ const App: FC = () => {
         <div className="flex flex-col gap-6">
           <Header />
           <main>
-            <UploadPage />
+            {!isHealthy && (
+              <Alert variant="destructive">
+                <AlertTitle>We couldn't reach the server</AlertTitle>
+                <AlertDescription>
+                  Please start the mock server and reload the page.
+                </AlertDescription>
+              </Alert>
+            )}
+
+            {isHealthy && <UploadPage />}
           </main>
         </div>
       </div>
