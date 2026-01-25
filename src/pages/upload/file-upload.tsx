2cb8602dd48a07dec3e707f28717fd35710c9ddc
Nikita Sirotin <Nikita.Sirotin@x5.ru>
2026-01-25 18:48:52 +0300

    style: fix overflow behaviour of main layout

--- src/pages/upload/file-upload.tsx
+++ src/pages/upload/file-upload.tsx
@@ -297,7 +297,7 @@ const FileUpload: FC<Props> = () => {
   })();
 
   return (
-    <div className="flex h-full w-full flex-col gap-6 rounded-xl border border-gray-800 bg-gray-900/40 p-6">
+    <div className="flex w-full flex-col gap-6 rounded-xl border border-gray-800 bg-gray-900/40 p-6">
       <span className="text-h4 text-gray-50">File To Convert</span>
 
       <FileDropzone
